f391a9f57541b2a4c2b30e9393dbe2c2ac93d832
Divyansh Mishra <satyam07july@gmail.com>
2026-05-19 22:43:36 +0530

    popularcourse fix

--- frontend/src/pages/landing/LandingPage.jsx
+++ frontend/src/pages/landing/LandingPage.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
-import PopularCourses from '../components/PopularCourses';
+import PopularCourses from "../../components/PopularCourses";
 import Features from "../../components/landing/Features";
 import Testimonials from "../../components/landing/Testimonials";
 import Footer from "../../components/landing/Footer";
