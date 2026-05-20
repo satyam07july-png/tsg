b3e117c34d838c76d70db7d5375b83c8700c9aa9
Divyansh Mishra <satyam07july@gmail.com>
2026-05-20 16:16:30 +0530

    Fixed PostgreSQL connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,35 +1,18 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-
-  connectionString:
-    process.env.DATABASE_URL,
-
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
-
 });
 
-// TEST CONNECTION
-
-pool.query("SELECT NOW()")
-
+pool.connect()
   .then(() => {
-
-    console.log(
-      "Database Connected ✅"
-    );
-
+    console.log("PostgreSQL Connected ✅");
   })
-
   .catch((err) => {
-
-    console.log(
-      "Database Connection Error ❌",
-      err
-    );
-
+    console.log("DB Connection Error ❌", err);
   });
 
 module.exports = pool;
